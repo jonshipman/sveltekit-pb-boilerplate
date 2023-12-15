9a9ba6bb1168089c221f4c7500332d5061852e1f
Jon Shipman <jon@jonshipman.com>
2023-12-14 19:17:40 -0600

    postcss and tailwindcss setup

---
+++ [web]/src/routes/+layout.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	import './layout.css';
+</script>
+
+<slot />
---
+++ [web]/tailwind.config.js
@@ -0,0 +1,6 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ["./src/**/*.{html,js,svelte,ts}"],
+	theme: {},
+	plugins: [],
+};
