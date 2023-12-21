16ad7ae729ca0c75aa714899389fedfcfaa37622
Jon Shipman <jon@jonshipman.com>
2023-12-21 08:51:19 -0600

    multiplatform setup, added pocketbase download, cleanup

--- setup.cmd
+++ setup.cmd
@@ -1,12 +1,4 @@
 @ECHO OFF
 SETLOCAL ENABLEDELAYEDEXPANSION
 
-CALL npm create svelte@latest web
-CHDIR web
-CALL npm add -D postcss tailwindcss autoprefixer @sveltejs/adapter-node
-CALL npx tailwindcss init
-CALL npm remove @sveltejs/adapter-auto
-@REM https://fart-it.sourceforge.net/
-CALL fart.exe web\svelte.config.js adapter-auto adapter-node
-CHDIR ..
-ROBOCOPY [web]\ web\ /E
+CALL node setup.mjs
---
+++ setup.mjs
@@ -0,0 +1,100 @@
+import cp from 'node:child_process';
+import fs from 'node:fs';
+import os from 'node:os';
+import path from 'node:path';
+
+const PBVERSION = '0.20.1';
+
+const pblink = getPocketbaseLink();
+const pbzippath = path.join(os.tmpdir(), 'pocketbase.zip');
+await run('curl', ['-o', pbzippath, '-L', pblink]);
+
+if (os.platform() == 'win32') {
+	await run('TAR.EXE', ['-xf', pbzippath], { cwd: os.tmpdir() });
+	await run('MOVE', [path.join(os.tmpdir(), 'pocketbase.exe'), 'db\\']);
+	await run('DEL', ['/F', pbzippath]);
+} else {
+	await run('unzip', [pbzippath], { cwd: os.tmpdir() });
+	await run('mv', [path.join(os.tmpdir(), 'pocketbase'), 'db/']);
+	await run('rm', ['-f', pbzippath]);
+}
+
+await run('npm', ['create', 'svelte@latest', 'web']);
+await run('npm', [
+	'--prefix',
+	'web',
+	'add',
+	'-D',
+	'postcss',
+	'tailwindcss',
+	'autoprefixer',
+	'@sveltejs/adapter-node'
+]);
+await run('npx', ['tailwindcss', 'init'], { cwd: 'web' });
+await run('npm', ['--prefix', 'web', 'remove', '@sveltejs/adapter-auto']);
+
+const configFile = path.join('web', 'svelte.config.js');
+const config = await fs.promises.readFile(configFile, 'utf8');
+const contents = config.replace('adapter-auto', 'adapter-node');
+await fs.promises.writeFile(configFile, contents, 'utf8');
+
+if (os.platform() == 'win32') {
+	await run('ROBOCOPY.EXE', ['[web]\\', 'web\\', '/E']);
+	await run('RMDIR', ['[web]', '/S', '/Q']);
+	await run('RMDIR', ['.git', '/S', '/Q']);
+	await run('DEL', ['/F', 'README.md']);
+	await run('DEL', ['/F', 'setup.*']);
+} else {
+	await run('rsync', ['-av', '[web]/', 'web/']);
+	await run('rm', ['-rf', '[web]']);
+	await run('rm', ['-rf', '.git']);
+	await run('rm', ['-f', 'setup.*']);
+}
+
+await run('git', ['init']);
+await run('git', ['add', '.']);
+
+function run(command, args = [], options = {}) {
+	console.log('\x1b[43m', 'command', '\x1b[0m');
+	console.log('\x1b[33m', command, args.join(' '), '\x1b[0m', '\n');
+
+	return new Promise((resolve, reject) => {
+		options.cwd = options.cwd || '.';
+		options.shell = options.shell ?? true;
+		options.execPath = os.platform() === 'win32' ? 'CMD.EXE' : 'sh';
+		options.execArgv = os.platform() === 'win32' ? ['/C'] : [];
+
+		const p = cp.fork(command, args, options);
+		let data = '';
+		p.on('message', (m) => {
+			data += m;
+		});
+
+		p.on('error', () => {
+			reject(data);
+		});
+
+		p.on('close', () => {
+			resolve(data);
+		});
+	});
+}
+
+function getPocketbaseLink() {
+	let platform = 'linux';
+	let arch = 'amd64';
+
+	if (os.arch() === 'arm64') {
+		arch = 'arm64';
+	} else if (os.arch() === 'armv7') {
+		arch = 'armv7';
+	}
+
+	if (os.platform() == 'win32') {
+		platform = 'windows';
+	} else if (os.platform() === 'darwin') {
+		platform = 'darwin';
+	}
+
+	return `https://github.com/pocketbase/pocketbase/releases/download/v${PBVERSION}/pocketbase_${PBVERSION}_${platform}_${arch}.zip`;
+}
