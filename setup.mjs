c0e5a2da0c125ba5e0e2dd52c16b38055a16327e
Jon Shipman <jon@jonshipman.com>
2024-01-04 18:00:04 -0600

    pb update

--- setup.mjs
+++ setup.mjs
@@ -3,7 +3,7 @@ import fs from 'node:fs';
 import os from 'node:os';
 import path from 'node:path';
 
-const PBVERSION = '0.20.4';
+const PBVERSION = '0.20.5';
 const BASENAME = path.basename(process.cwd());
 
 const pblink = getPocketbaseLink();
