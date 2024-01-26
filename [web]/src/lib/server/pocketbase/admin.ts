971d8a6b8082fddcd472463c3377e0c86a289333
Jon Shipman <jon@jonshipman.com>
2024-01-25 19:43:11 -0600

    admin instance caches to prevent stacked logins

--- [web]/src/lib/server/pocketbase/admin.ts
+++ [web]/src/lib/server/pocketbase/admin.ts
@@ -2,17 +2,26 @@ import { PUBLIC_DATABASE } from '$env/static/public';
 import { ADMIN_USER, ADMIN_PASS } from '$env/static/private';
 import Pocketbase from 'pocketbase';
 
+let PCKBS: Pocketbase | undefined = undefined;
+let PB_EXPIRES = Date.now();
+
 /**
  * Pocketbase instance authenticated as an admin using `ADMIN_USER` and `ADMIN_PASS` envvars.
  */
 export async function pbAsAdmin() {
+	if (PCKBS && PB_EXPIRES > Date.now() && PCKBS.authStore.isValid) return PCKBS;
+
 	const pb = new Pocketbase(PUBLIC_DATABASE);
 	await pb.admins.authWithPassword(ADMIN_USER, ADMIN_PASS);
 
 	if (pb.authStore.isValid) {
+		PCKBS = pb;
+		PB_EXPIRES = Date.now() + 3600;
 		return pb;
 	}
 
 	pb.authStore.clear();
+	if (PCKBS) PCKBS.authStore.clear();
+
 	throw new Error('ADMIN_USER/ADMIN_PASS not valid');
 }
---
+++ [web]/src/lib/server/pocketbase/index.ts
@@ -0,0 +1,1 @@
+export { pbAsAdmin } from './admin';
